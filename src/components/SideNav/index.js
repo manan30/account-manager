fadd159c736c72147c11271f1cdd65f91a3a127e
Manan Joshi <mananjoshi1995@gmail.com>
2020-11-21 16:28:29 -0600

    Adding styling to nav links

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import cn from 'classnames';
 
 const links = [
   { to: '/', linkText: 'Overview' },
@@ -9,12 +10,20 @@ const links = [
 ];
 
 function SideNav() {
+  const { pathname } = useLocation();
   return (
     <div className='bg-gray-300 px-8'>
       <h2 className='text-3xl mt-6 text-gray-800'>Account Manager</h2>
       <nav className='flex flex-col'>
         {links.map((link) => (
-          <Link to={link.to} className='mt-5 text-xl text-indigo-600'>
+          <Link
+            to={link.to}
+            className={cn(
+              'mt-5 text-xl text-indigo-600',
+              'hover:opacity-100',
+              pathname !== link.to && 'opacity-50'
+            )}
+          >
             {link.linkText}
           </Link>
         ))}
