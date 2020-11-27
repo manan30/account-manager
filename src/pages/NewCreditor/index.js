6cbe14159473a68c126e5908c0adc9bd5a76d8e0
Manan Joshi <mananjoshi1995@gmail.com>
2020-11-27 16:46:48 -0600

    Adding new creditor page

---
+++ src/pages/NewCreditor/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+function NewCreditor() {
+  return (
+    <div className='flex justify-center w-full'>
+      <form className='my-8 w-1/3'>
+        <div className='mt-6'>
+          <Input
+            name='name'
+            placeHolder='Name of person or entity'
+            label='Name'
+          />
+        </div>
+        <div className='mt-6'>
+          <Input name='amount' type='tel' placeHolder='$0.00' label='Amount' />
+        </div>
+        <div className='mt-6'>
+          <Select
+            name='currency'
+            type='tel'
+            placeHolder='USD, INR, etc'
+            label='Currency'
+            selectOptions={['USD', 'CAD', 'INR']}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default NewCreditor;
