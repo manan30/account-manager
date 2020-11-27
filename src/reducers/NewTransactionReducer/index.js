2feed748d3d633834e73226a3e626b90d4f5c7d9
Manan Joshi <mananjoshi1995@gmail.com>
2020-11-27 11:25:32 -0600

    Adding action type to reducer

--- src/reducers/NewTransactionReducer/index.js
+++ src/reducers/NewTransactionReducer/index.js
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import { ADD_AMOUNT, ADD_TRANSACTION_TYPE } from '../../utils/Constants';
 
 const newTransactionInitialState = {
   transactionType: '',
@@ -7,6 +8,10 @@ const newTransactionInitialState = {
 
 const newTransactionReducer = (state, action) => {
   switch (action.type) {
+    case ADD_TRANSACTION_TYPE:
+      return { ...state, transactionType: action.payload };
+    case ADD_AMOUNT:
+      return { ...state, amount: action.payload };
     default:
       throw new Error('Unknown action');
   }
---
+++ src/utils/Constants/NewTransactionReducerActionTypes.js
@@ -0,0 +1,3 @@
+export const ADD_TRANSACTION_TYPE = 'ADD_TRANSACTION_TYPE';
+
+export const ADD_AMOUNT = 'ADD_AMOUNT';
