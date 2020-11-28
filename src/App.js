b7305ee1434c018d909a7a09a94e185f49f1caed
Manan Joshi <mananjoshi1995@gmail.com>
2020-11-28 02:01:39 -0600

    Extracting routes into its own component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SideNav from './components/SideNav';
-
-const routes = [
-  { path: '/', component: React.lazy(() => import('./pages/Overview')) },
-  {
-    path: '/new-transaction',
-    component: React.lazy(() => import('./pages/NewTransaction/withContext.js'))
-  },
-  {
-    path: '/new-creditor',
-    component: React.lazy(() => import('./pages/NewCreditor'))
-  }
-];
+import React from 'react';
+import AppRouter from './Router';
+import FirebaseProvider from './contexts/FirebaseContext';
 
 function App() {
   return (
-    <Router>
-      <div className='flex h-full w-full'>
-        <SideNav />
-        <Suspense fallback={<div />}>
-          <Switch>
-            {routes.map((route, i) => {
-              const key = i;
-              return (
-                <Route
-                  exact
-                  key={key}
-                  path={route.path}
-                  component={route.component}
-                />
-              );
-            })}
-          </Switch>
-        </Suspense>
-      </div>
-    </Router>
+    <FirebaseProvider>
+      <AppRouter />
+    </FirebaseProvider>
   );
 }
 
