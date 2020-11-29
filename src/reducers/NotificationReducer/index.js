787a13a900e884c6ae66f1c0be4b0f961a8c970f
Manan Joshi <mananjoshi1995@gmail.com>
2020-11-29 01:05:27 -0600

    Adding different action types to notification reducer

--- src/reducers/NotificationReducer/index.js
+++ src/reducers/NotificationReducer/index.js
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import { ADD_NOTIFICATION, REMOVE_NOTIFICATION } from '../../utils/Constants';
 
 const notificationDefaultState = {
   content: '',
@@ -8,6 +9,14 @@ const notificationDefaultState = {
 
 const notificationReducer = (state, action) => {
   switch (action.type) {
+    case ADD_NOTIFICATION:
+      return {
+        content: action.payload.content,
+        theme: action.payload.theme,
+        showNotification: true
+      };
+    case REMOVE_NOTIFICATION:
+      return { ...state, showNotification: false };
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
--- src/utils/Constants/index.js
+++ src/utils/Constants/index.js
@@ -1,1 +1,2 @@
 export * from './NewTransactionReducerActionTypes';
+export * from './NotificationReducerActionTypes';
