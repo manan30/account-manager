9c19d45361415258afed4a6d7812ee5cf637b3ed
Manan Joshi <mananjoshi1995@gmail.com>
2020-12-04 01:23:11 -0600

    Finishing migration to TypeScript

--- src/pages/NewCreditor/index.tsx
+++ src/pages/NewCreditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
