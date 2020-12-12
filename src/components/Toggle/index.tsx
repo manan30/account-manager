dcef22ddce9f2210d3ca0f47a593da8f10b522d7
Manan Joshi <mananjoshi1995@gmail.com>
2020-12-12 13:46:13 -0600

    Animating on/off states of toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -3,15 +3,25 @@ import cn from 'classnames';
 
 const Toggle = () => {
   const [status, setStatus] = useState(false);
+  const [firstClick, setFirstClick] = useState(false);
+
+  const handleClick = () => {
+    if (!firstClick) setFirstClick(true);
+    setStatus((prevState) => !prevState);
+  };
 
   return (
     <div className='h-4 w-12 rounded-lg bg-indigo-300 flex items-center shadow'>
       <button
         className={cn(
-          'h-6 w-6 rounded-full',
-          status ? 'animate-toggle-on' : 'animate-toggle-off'
+          'h-6 w-6 rounded-full bg-red-500',
+          firstClick
+            ? status
+              ? 'transition-transform transform translate-x-6 duration-500 ease-out'
+              : 'transition-transform transform translate-x-0 duration-500 ease-out'
+            : 'bg-indigo-600'
         )}
-        onClick={() => setStatus((prevState) => !prevState)}
+        onClick={handleClick}
       ></button>
     </div>
   );
