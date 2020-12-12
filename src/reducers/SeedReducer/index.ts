62eca37eca604e62eea73e9ab3cf9dc2ae67779a
Manan Joshi <mananjoshi1995@gmail.com>
2020-12-11 19:02:16 -0600

    Starting useSeedReducer hook

--- src/reducers/SeedReducer/index.ts
+++ src/reducers/SeedReducer/index.ts
@@ -1,6 +1,12 @@
-import { ISeedState } from './seedReducer.interface';
+import { useReducer } from 'react';
+import {
+  ISeedAction,
+  ISeedState,
+  CLEAR_EVERYTHING,
+  CLEAR_CREDITORS
+} from './seedReducer.interface';
 
-const SeedDefaultState: ISeedState = {
+const seedDefaultState: ISeedState = {
   delete: {
     everything: false,
     creditors: false
@@ -10,3 +16,22 @@ const SeedDefaultState: ISeedState = {
     creditors: false
   }
 };
+
+const seedReducer = (state: ISeedState, action: ISeedAction) => {
+  switch (action.type) {
+    case CLEAR_EVERYTHING:
+      return {
+        ...state,
+        delete: { ...state.delete, everything: true }
+      } as ISeedState;
+    case CLEAR_CREDITORS:
+      return {
+        ...state,
+        delete: { ...state.delete, creditors: true, everything: false }
+      } as ISeedState;
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
+export default () => useReducer(seedReducer, seedDefaultState);
