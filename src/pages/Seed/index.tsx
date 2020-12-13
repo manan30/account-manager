9fc0f426794c42056e4329c3216d8aab8addd579
Manan Joshi <mananjoshi1995@gmail.com>
2020-12-13 17:39:55 -0600

    Changing seed page styles

--- src/pages/Seed/index.tsx
+++ src/pages/Seed/index.tsx
@@ -3,7 +3,7 @@ import Toggle from '../../components/Toggle';
 import Button from '../../components/Button';
 import { useSeedDispatch, useSeedState } from '../../providers/SeedProvider';
 import { SeedActionType } from '../../reducers/SeedReducer/seedReducer.interface';
-import { deleteEverything } from '../../services/seed/seed';
+import { seedEverything } from '../../services/seed/seed';
 
 const Seed = () => {
   const { seedOptions } = useSeedState();
@@ -19,7 +19,7 @@ const Seed = () => {
       }
 
       if (seedOptions.everything) {
-        await deleteEverything();
+        await seedEverything();
       }
     } catch (e) {
       console.error(e);
@@ -31,31 +31,27 @@ const Seed = () => {
   return (
     <div className='w-full flex justify-items-center items-center flex-col p-6'>
       <h1 className='text-indigo-600 font-bold text-5xl mt-8'>Seeder</h1>
-      <div className='flex w-full min-h-c-8 mt-8 px-16'>
-        <div className='p-4'>
-          <div className='flex flex-col px-10'>
-            {Object.entries(seedOptions).map(([key, value]) => {
-              return (
-                <div className='flex items-center mt-4' key={key}>
-                  <h2 className='text-green-500 font-medium text-xl flex-auto'>
-                    Seed {key[0].toUpperCase() + key.slice(1)}
-                  </h2>
-                  <div>
-                    <Toggle
-                      value={value}
-                      handleToggle={(state) => {
-                        seedDispatch({
-                          type: `ADD_${key.toUpperCase()}` as SeedActionType,
-                          payload: { status: state }
-                        });
-                      }}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+      <div className='flex w-1/2 min-h-c-8 mt-8 px-16 flex-col'>
+        {Object.entries(seedOptions).map(([key, value]) => {
+          return (
+            <div className='flex items-center mt-4' key={key}>
+              <h2 className='text-indigo-400 font-medium text-xl flex-auto'>
+                Seed {key[0].toUpperCase() + key.slice(1)}
+              </h2>
+              <div>
+                <Toggle
+                  value={value}
+                  handleToggle={(state) => {
+                    seedDispatch({
+                      type: `ADD_${key.toUpperCase()}` as SeedActionType,
+                      payload: { status: state }
+                    });
+                  }}
+                />
+              </div>
+            </div>
+          );
+        })}
       </div>
       <div className='w-1/4 mt-8'>
         <Button
--- src/services/seed/seed.ts
+++ src/services/seed/seed.ts
@@ -2,21 +2,8 @@ import FirebaseService from '../firebase';
 
 const { firestore } = FirebaseService();
 
-const deleteCollection = async (collectionName: string) => {
-  const collectionRef = firestore.collection(collectionName);
-  const snapshot = await collectionRef.get();
-
-  if (snapshot.size > 0) {
-    const batch = firestore.batch();
-    snapshot.docs.forEach((doc) => {
-      batch.delete(doc.ref);
-    });
-    await batch.commit();
-  }
-};
-
-export const deleteEverything = async () => {
-  await deleteCollection('creditors');
-};
-
 export const seedCreditors = async () => {};
+
+export const seedEverything = async () => {
+  await seedCreditors();
+};
