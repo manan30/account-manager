ec76e47080ed5f32a0bb6f1eab308045249021ff
Manan Joshi <mananjoshi1995@gmail.com>
2020-12-13 18:45:19 -0600

    Adding first fake data

--- src/services/seed/seed.ts
+++ src/services/seed/seed.ts
@@ -1,9 +1,18 @@
 import FirebaseService from '../firebase';
+import faker from 'faker';
 
-const { firestore } = FirebaseService();
+const { firestore, firebaseApp } = FirebaseService();
 
 export const seedCreditors = async () => {
-  
+  const amount = faker.finance.amount(0, 3000);
+  await firestore?.collection('creditors').add({
+    name: faker.name.findName(),
+    amount: amount,
+    currency: faker.finance.currencyCode(),
+    remainingAmount: amount,
+    createdAt: firebaseApp?.firestore.Timestamp.now(),
+    accountSettledOn: null
+  });
 };
 
 export const seedEverything = async () => {
