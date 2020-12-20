4897d2a41fdac969477282f023ad41c339106abc
Manan Joshi <mananjoshi1995@gmail.com>
2020-12-20 16:04:07 -0600

    Purging unused css styles

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
     require('autoprefixer'),
     process.env.NODE_ENV === 'production' &&
       require('@fullhuman/postcss-purgecss')({
-        content: ['./src/**/*.js', './public/index.html'],
+        content: ['./src/**/*.tsx', './public/index.html'],
         defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
       })
   ].filter(Boolean)
