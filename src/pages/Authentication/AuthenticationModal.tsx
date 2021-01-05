8647c88e4819654b072ccfcb1e354652a204573c
Manan Joshi <mananjoshi1995@gmail.com>
2021-01-05 01:26:19 -0600

    Adding Twitter icon

--- src/pages/Authentication/AuthenticationModal.tsx
+++ src/pages/Authentication/AuthenticationModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from '../../components/Modal';
 import GoogleIcon from '../../assets/google-icon.svg';
+import TwitterIcon from '../../assets/twitter-icon.svg';
 
 const AuthenticationModal = () => {
   return (
@@ -19,11 +20,13 @@ const AuthenticationModal = () => {
             Sign in with Google
           </span>
         </button>
-        <button className='w-full shadow rounded-md p-2 mt-3'>
-          Sign in with Twitter
-        </button>
-        <button className='w-full shadow rounded-md p-2 mt-3'>
-          Sign in with Facebook
+        <button className='w-full shadow rounded-md p-2 flex items-center justify-center mt-3 bg-blue-500'>
+          <div className='h-6 w-6 mr-4'>
+            <TwitterIcon />
+          </div>
+          <span className='tracking-wide font-semibold text-sm text-white'>
+            Sign in with Twitter
+          </span>
         </button>
         <button className='w-full shadow rounded-md p-2 mt-3'>
           Sign in with Apple
