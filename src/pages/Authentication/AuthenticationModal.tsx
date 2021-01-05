e714087bcb037cd7e58c6c31fb2ff3486df5dd38
Manan Joshi <mananjoshi1995@gmail.com>
2021-01-05 01:53:56 -0600

    Removing Twitter and Apple sign in

--- src/pages/Authentication/AuthenticationModal.tsx
+++ src/pages/Authentication/AuthenticationModal.tsx
@@ -20,17 +20,6 @@ const AuthenticationModal = () => {
             Sign in with Google
           </span>
         </button>
-        <button className='w-full shadow rounded-md p-2 flex items-center justify-center mt-3 bg-blue-500'>
-          <div className='h-6 w-6 mr-4'>
-            <TwitterIcon />
-          </div>
-          <span className='tracking-wide font-semibold text-sm text-white'>
-            Sign in with Twitter
-          </span>
-        </button>
-        <button className='w-full shadow rounded-md p-2 mt-3'>
-          Sign in with Apple
-        </button>
       </div>
     </Modal>
   );
