0a9faf5d63e43451ff7f3807b7540f02cd42f0b3
Manan Joshi <mananjoshi1995@gmail.com>
2021-01-26 17:12:02 -0600

    Adding hook for fetching spending data

---
+++ src/hooks/Spending/useGetSpendingData.ts
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useFirebaseContext } from '../../providers/FirebaseProvider';
+import { ISpending } from 'models/Spending';
+
+// TODO: Combine both the creditor hooks
+const useGetSpendingData = () => {
+  const { firestore } = useFirebaseContext();
+  const [data, setData] = useState<ISpending[] | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchSpendingData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      setError(false);
+      const spendingDBRef = firestore
+        ?.collection('spending')
+        .orderBy('updatedAt', 'desc');
+
+      const queryDocs = await spendingDBRef?.get();
+      const spendingData = queryDocs?.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        } as ISpending;
+      });
+      setData(spendingData);
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [firestore]);
+
+  useEffect(() => {
+    fetchSpendingData();
+  }, [fetchSpendingData]);
+
+  return { data, error, isLoading };
+};
+
+export default useGetSpendingData;
--- src/pages/Spending/index.tsx
+++ src/pages/Spending/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import useGetSpendingData from '../../hooks/Spending/useGetSpendingData';
 
 const Spending = () => {
-  return <div>Hello</div>;
+  const { data: spendingData, isLoading, error } = useGetSpendingData();
+
+  return (
+    <>
+      <Helmet>
+        <title>{`Account Manager - Spending`}</title>
+        <meta name='title' content={`Account Manager - Spending`} />
+        <meta property='og:title' content={`Account Manager - Spending`} />
+        <meta property='twitter:title' content={`Account Manager - Spending`} />
+      </Helmet>
+    </>
+  );
 };
 
 export default Spending;
