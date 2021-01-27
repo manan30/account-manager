f07759bd15495e01380be5a915482081844194b1
Manan Joshi <mananjoshi1995@gmail.com>
2021-01-27 00:26:48 -0600

    gracefully exiting function on error

--- functions/src/Stores/addNewStore.ts
+++ functions/src/Stores/addNewStore.ts
@@ -6,17 +6,23 @@ import * as cors from 'cors';
 if (!admin.apps.length) admin.initializeApp();
 else admin.app();
 
-// const db = admin.firestore();
+const db = admin.firestore();
 
 const expressApp = express();
 expressApp.use(cors({ origin: true }));
 expressApp.post('/', async (req, res) => {
-  const { storeName } = req.body;
-  if (!storeName || storeName.trim() === '')
-    res.status(400).send({ error: 'Store name cannot be empty' });
+  try {
+    const { storeName } = req.body;
+    if (!storeName || storeName.trim() === '')
+      res.status(400).send({ error: 'Store name cannot be empty' });
 
-  console.log({ storeName });
-  res.send(200);
+    await db.collection('stores').add(storeName);
+
+    res.status(200).send({ message: 'New store was added successfully' });
+  } catch (err) {
+    console.error({ err });
+    res.status(500).send({ error: err.toString() });
+  }
 });
 
 export const addNewStore = functions.https.onRequest(expressApp);
