c6d76b603b250546958bb7918d551a038bb9c283
Manan Joshi <mananjoshi1995@gmail.com>
2021-02-09 01:44:22 -0600

    Adding trigger for updating transaction collection

---
+++ functions/src/Spending/index.ts
@@ -0,0 +1,3 @@
+import { createTransactionOnSpendingAddition } from './createTransactionOnSpendingAddition';
+
+export { createTransactionOnSpendingAddition };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as TransactionFunctions from './Transaction';
 import * as CreditorFunctions from './Creditor';
+import * as SpendingFunctions from './Spending';
 import { addNewStore } from './HttpFunctions/addNewStore';
 import { addNewSpendingCategory } from './HttpFunctions/addNewSpendingCategory';
 
 export {
   TransactionFunctions,
   CreditorFunctions,
+  SpendingFunctions,
   addNewStore,
   addNewSpendingCategory
 };
