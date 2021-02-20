21bd80b02961d622c1e01c4e9e71104fbe5e7fdc
Manan Joshi <mananjoshi1995@gmail.com>
2021-02-20 16:25:29 -0600

    Syncing new tailwind config with old

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,56 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      maxHeight: {
+        table: '40rem',
+        select: '10rem'
+      },
+      minHeight: {
+        c: '8rem'
+      },
+      width: {
+        select: 'calc(100% - 2rem)',
+        notification: '20rem'
+      },
+      animation: {
+        'notification-entry': 'notification-entry 0.5s ease-out',
+        'notification-exit': 'notification-exit 0.5s ease-in',
+
+        'modal-entry': 'modal-entry 0.6s ease-out',
+        'modal-exit': 'modal-exit 0.6s ease-out'
+      },
+      keyframes: {
+        'notification-entry': {
+          entry: {
+            '0%': {
+              transform: 'translateX(20rem)'
+            },
+            '100%': {
+              transform: 'translateX(0)'
+            }
+          },
+          exit: {
+            '0%': {
+              transform: 'translateX(0)'
+            },
+            '100%': {
+              transform: 'translateX(30rem)'
+            }
+          }
+        },
+        'modal-entry': {
+          '0%': {
+            transform: 'translateY(20rem)',
+            opacity: '0'
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1'
+          }
+        }
+      }
+    }
   },
   variants: {
     extend: {}
