e5b543253553a6174cb2f77ca5e253de06375f75
Manan Joshi <mananjoshi1995@gmail.com>
2021-03-03 23:06:29 -0600

    Adding plaid secrets to functions dir

--- functions/src/HttpFunctions/Accounts/index.ts
+++ functions/src/HttpFunctions/Accounts/index.ts
@@ -1,10 +1,14 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
-import { Client } from 'plaid';
+import { Client, ClientConfigs } from 'plaid';
 
 const expressApp = express();
 expressApp.use(cors({ origin: true }));
+expressApp.use(express.json());
+
+const plaidClientConfig: ClientConfigs = {};
+const plaidClient = new Client(plaidClientConfig);
 
 // expressApp.
 export const accounts = functions.https.onRequest(handler);
