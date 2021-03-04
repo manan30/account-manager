52c3e2d0605ce128461ff60f40e31592c26142ee
Manan Joshi <mananjoshi1995@gmail.com>
2021-03-04 00:26:45 -0600

    Opening Plaid Link dialog

--- src/pages/Accounts/index.tsx
+++ src/pages/Accounts/index.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
-import { PlaidLink } from 'react-plaid-link';
+import React, { useEffect, useState } from 'react';
+import { usePlaidLink } from 'react-plaid-link';
 
 const Accounts = () => {
+  const [linkToken, setLinkToken] = useState<null | string>(null);
+  const onSuccess = React.useCallback((public_token, metadata) => {
+    // send public_token to server
+    const response = fetch(
+      'http://localhost:5001/account-manager-41694/us-central1/accounts/plaid/set-access-token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ public_token })
+      }
+    );
+    // Handle response ...
+  }, []);
+
+  const config: Parameters<typeof usePlaidLink>[0] = {
+    token: linkToken || '',
+    onSuccess
+  };
+  const { open, ready } = usePlaidLink(config);
+
+  const generateToken = async () => {
+    const response = await fetch(
+      'http://localhost:5001/account-manager-41694/us-central1/accounts/plaid/create-link-token',
+      { method: 'POST' }
+    );
+    const data = await response.json();
+    console.log({ data });
+    setLinkToken(data.link_token);
+  };
+
+  useEffect(() => {
+    generateToken();
+  }, []);
+
   return (
     <div>
       Accounts Page
-      <PlaidLink
-        onSuccess={(props) => {
-          console.log({ props });
-        }}
-      />
+      {linkToken && (
+        <button onClick={() => open()} disabled={!ready}>
+          Link account
+        </button>
+      )}
     </div>
   );
 };
