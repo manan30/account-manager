b75ba5e172383bb8a09bdca75299baff0078ec6e
Manan Joshi <mananjoshi1995@gmail.com>
2021-03-03 23:28:22 -0600

    Creating plaid client

--- functions/src/HttpFunctions/Accounts/index.ts
+++ functions/src/HttpFunctions/Accounts/index.ts
@@ -1,13 +1,18 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
-import { Client, ClientConfigs } from 'plaid';
+import { Client, ClientConfigs, environments } from 'plaid';
 
 const expressApp = express();
 expressApp.use(cors({ origin: true }));
 expressApp.use(express.json());
 
-const plaidClientConfig: ClientConfigs = {};
+const plaidClientConfig: ClientConfigs = {
+  clientID: functions.config().plaid.client_id,
+  secret: functions.config().plaid.sandbox_secret,
+  env: environments.sandbox,
+  options: { version: '2020-09-14' }
+};
 const plaidClient = new Client(plaidClientConfig);
 
 // expressApp.
