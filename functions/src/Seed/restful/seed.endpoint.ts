939740ebd524095a2a23817c9a6775a955b444ad
Manan Joshi <mananjoshi1995@gmail.com>
2021-04-05 23:30:25 -0500

    Adding support for seeding spending data

--- functions/src/Seed/restful/seed.endpoint.ts
+++ functions/src/Seed/restful/seed.endpoint.ts
@@ -3,7 +3,10 @@ import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
 import { SeedRequestBody } from '../interfaces/seed.interface';
-import { generateFakeCreditor } from '../utils/generateFakeData';
+import {
+  generateFakeCreditor,
+  generateFakeSpending
+} from '../utils/generateFakeData';
 import { BankTransactionCollection } from '../../BankTransactions/interfaces/bankTransaction.model';
 import { SpendingCollection } from '../../Spending/interfaces/spending.model';
 import { CreditorCollection } from '../../Creditor/interfaces/creditor.model';
@@ -62,27 +65,39 @@ expressApp.post('/', async (req, res) => {
       await batch.commit();
     }
 
+    if (seedOptions.spending) {
+      const { clear: clearSpending, count } = seedOptions.spending;
+      const spendingDbRef = db.collection(SpendingCollection);
+
+      const spendingDbSnapshot = await spendingDbRef.get();
+
+      if (clearSpending) {
+        const batch = db.batch();
+        spendingDbSnapshot.docs.forEach((doc) => {
+          batch.delete(doc.ref);
+        });
+        await batch.commit();
+      }
+
+      const spendingCount = count ?? 10;
+      const batch = db.batch();
+
+      for (let i = 0; i < spendingCount; i += 1) {
+        const creditor = generateFakeSpending();
+        const id = db.collection(SpendingCollection).doc().id;
+        const docRef = db.collection(SpendingCollection).doc(id);
+        batch.set(docRef, creditor);
+      }
+
+      await batch.commit();
+    }
+
     if (seedOptions.bankTransactions) {
       const { clear: clearBankTransactions } = seedOptions.bankTransactions;
       const bankTransactionsDbRef = db.collection(BankTransactionCollection);
       const snapshot = await bankTransactionsDbRef.get();
 
       if (clearBankTransactions) {
-        const batch = db.batch();
-        snapshot.docs.forEach((doc) => {
-          batch.delete(doc.ref);
-        });
-        await batch.commit();
-      }
-    }
-
-    if (seedOptions.spending) {
-      const { clear: clearSpending } = seedOptions.spending;
-      const spendingDbRef = db.collection(SpendingCollection);
-
-      const snapshot = await spendingDbRef.get();
-
-      if (clearSpending) {
         const batch = db.batch();
         snapshot.docs.forEach((doc) => {
           batch.delete(doc.ref);
--- functions/src/Seed/utils/generateFakeData.ts
+++ functions/src/Seed/utils/generateFakeData.ts
@@ -1,6 +1,7 @@
 import * as faker from 'faker';
 import * as admin from 'firebase-admin';
 import { Creditor } from '../../Creditor/interfaces/creditor.model';
+import { Spending } from '../../Spending/interfaces/spending.model';
 
 export const generateFakeCreditor = (): Creditor => {
   const name = faker.name.findName();
@@ -19,4 +20,54 @@ export const generateFakeCreditor = (): Creditor => {
     accountSettled: false,
     updatedAt: createdAt
   } as Creditor;
+};
+
+const categories = [
+  'accommodation',
+  'advertising',
+  'bar',
+  'charity',
+  'clothing',
+  'dining',
+  'education',
+  'electronics',
+  'entertainment',
+  'fuel',
+  'groceries',
+  'health',
+  'home',
+  'income',
+  'insurance',
+  'investment',
+  'loan',
+  'office',
+  'phone',
+  'service',
+  'shopping',
+  'software',
+  'sport',
+  'tax',
+  'transport',
+  'transportation',
+  'utilities'
+];
+
+export const generateFakeSpending = (): Spending => {
+  const storeName = faker.company.companyName();
+  const category =
+    categories[Math.floor(Math.random() * (categories.length - 1))];
+  const amount = Number(faker.commerce.price());
+  const date = admin.firestore.Timestamp.fromDate(
+    faker.date.past(2, '12/31/2021')
+  );
+  const createdAt = admin.firestore.Timestamp.now();
+
+  return {
+    storeName,
+    category,
+    amount,
+    date,
+    createdAt,
+    updatedAt: createdAt
+  } as Spending;
 };
