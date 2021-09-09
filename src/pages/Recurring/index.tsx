99e609833e6e14a91c08a8777e27ad4efde439db
Manan Joshi <mananjoshi1995@gmail.com>
2021-09-09 17:38:57 -0500

    Splitting transactions into upcoming and completed

--- src/pages/Recurring/index.tsx
+++ src/pages/Recurring/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense, useMemo, useState } from 'react';
 import { PlusIcon } from '@heroicons/react/solid';
 import FloatingActionButton from '../../components/Button/FloatingActionButton';
 import RecurringEntryModal from './RecurringEntryModal';
@@ -14,9 +14,40 @@ const Recurring = () => {
     }
   );
 
+  const { upcomingTransactions, completedTransactions } = useMemo(() => {
+    const currentDate = Number(
+      Intl.DateTimeFormat('en-US', { day: 'numeric' }).format(new Date())
+    );
+    const upcomingTransactions: Array<RecurringModel> = [];
+    const completedTransactions: Array<RecurringModel> = [];
+
+    recurringTransactions?.forEach((transaction) => {
+      const recurringDate = Number(
+        Intl.DateTimeFormat('en-US', { day: 'numeric' }).format(
+          transaction.recurringDate.toDate()
+        )
+      );
+      if (recurringDate >= currentDate) upcomingTransactions.push(transaction);
+      else completedTransactions.push(transaction);
+    });
+    return { upcomingTransactions, completedTransactions };
+  }, [recurringTransactions]);
+
   return (
     <>
-      <pre>{JSON.stringify(recurringTransactions, null, 2)}</pre>
+      <section className='my-6'>
+        <h2 className='mb-6 text-xl font-semibold tracking-wide text-indigo-600'>
+          Upcoming
+        </h2>
+        <pre>{JSON.stringify(upcomingTransactions.length, null, 2)}</pre>
+        {/* <div>{recurringTransactions?.map((transaction) => )}</div> */}
+      </section>
+      <section className='my-6'>
+        <h2 className='mb-6 text-xl font-semibold tracking-wide text-indigo-600'>
+          Completed
+        </h2>
+        {JSON.stringify(completedTransactions.length, null, 2)}
+      </section>
       <div className='fixed bottom-0 right-0 mb-8 mr-8'>
         <FloatingActionButton
           icon={<PlusIcon className='w-6 h-6 text-gray-100' />}
