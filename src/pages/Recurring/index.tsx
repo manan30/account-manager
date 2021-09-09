9ba8af440c38ccf4828d1ce769d43259dd2f08d6
Manan Joshi <mananjoshi1995@gmail.com>
2021-09-09 16:53:56 -0500

    Getting recurring transactions from firestore

--- src/pages/Recurring/index.tsx
+++ src/pages/Recurring/index.tsx
@@ -3,12 +3,20 @@ import { PlusIcon } from '@heroicons/react/solid';
 import FloatingActionButton from '../../components/Button/FloatingActionButton';
 import RecurringEntryModal from './RecurringEntryModal';
 import ModalFallback from '../../components/ModalFallback';
+import useFirestoreReadQuery from '../../hooks/Firestore/useFirestoreReadQuery';
+import { Recurring as RecurringModel } from '../../models/Recurring';
 
 const Recurring = () => {
   const [openRecurringEntryModal, setOpenRecurringEntryModal] = useState(false);
+  const { data: recurringTransactions } = useFirestoreReadQuery<RecurringModel>(
+    {
+      collection: 'recurring'
+    }
+  );
 
   return (
     <>
+      <pre>{JSON.stringify(recurringTransactions, null, 2)}</pre>
       <div className='fixed bottom-0 right-0 mb-8 mr-8'>
         <FloatingActionButton
           icon={<PlusIcon className='w-6 h-6 text-gray-100' />}
