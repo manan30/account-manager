72aff0c983fa0a937180560659b1ef83fd802fe4
Manan Joshi <mananjoshi1995@gmail.com>
2021-12-24 11:30:27 -0500

    Completing password based authentication

--- src/services/firebase/hooks/usePasswordAuth.ts
+++ src/services/firebase/hooks/usePasswordAuth.ts
@@ -1,21 +1,39 @@
 import { useEffect, useState } from 'react';
+import { AuthError } from '@firebase/auth-types';
 import { useFirebaseContext } from '../../../providers/FirebaseProvider';
 import { useNotificationDispatch } from '../../../providers/NotificationProvider';
 import { NOTIFICATION_THEME_FAILURE } from '../../../utils/Constants/ThemeConstants';
+import { AUTH_ERROR_CODES } from '../constants';
 
 export const usePasswordAuth = () => {
   const notificationDispatch = useNotificationDispatch();
   const { auth } = useFirebaseContext();
-  const [error, setError] = useState(false);
+  const [error, setError] = useState<AuthError | null>(null);
   const [processing, setProcessing] = useState(false);
+
+  const signInWithEmail = async (email: string, password: string) => {
+    try {
+      setProcessing(true);
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (err) {
+      console.error(err);
+      setError(err as AuthError);
+    } finally {
+      setProcessing(false);
+    }
+  };
 
   const handlePasswordAuth = async (email: string, password: string) => {
     try {
       setProcessing(true);
       await auth.createUserWithEmailAndPassword(email, password);
     } catch (err) {
+      if ((err as AuthError).code === AUTH_ERROR_CODES.EMAIL_IN_USE) {
+        await signInWithEmail(email, password);
+        return;
+      }
       console.error(err);
-      setError(true);
+      setError(err as AuthError);
     } finally {
       setProcessing(false);
     }
@@ -26,7 +44,7 @@ export const usePasswordAuth = () => {
       notificationDispatch({
         type: 'ADD_NOTIFICATION',
         payload: {
-          content: 'Unable to create user. Please try again in some time',
+          content: error.message,
           theme: NOTIFICATION_THEME_FAILURE
         }
       });
