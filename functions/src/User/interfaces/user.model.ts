7b4eec5706d37942641380398f5977d64416f27c
Manan Joshi <mananjoshi1995@gmail.com>
2021-12-24 17:07:03 -0500

    Creating on user creation firebase trigger function

--- functions/src/User/interfaces/user.model.ts
+++ functions/src/User/interfaces/user.model.ts
@@ -1,14 +1,35 @@
 import { Timestamp } from '@firebase/firestore-types';
 import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 
-export interface ICreditor extends DocumentSnapshot {
+type UserMetadata = {
+  lastSignInTime: string;
+  creationTime: string;
+  lastRefreshTime?: string | null;
+};
+
+type UserInfo = {
+  uid: string;
+  displayName: string;
+  email: string;
+  phoneNumber: string;
+  photoURL: string;
+  providerId: string;
+};
+
+export interface User extends DocumentSnapshot {
   id: string;
-  name: string;
-  amount: number;
-  currency: string;
-  remainingAmount: number;
-  accountSettledOn?: string | null;
-  accountSettled: boolean;
+  uid: string;
+  email?: string;
+  emailVerified: boolean;
+  displayName?: string;
+  phoneNumber?: string;
+  photoURL?: string;
+  disabled: boolean;
+  metadata: UserMetadata;
+  providerData: Array<UserInfo>;
+  tenantId?: string | null;
   createdAt: Timestamp;
   updatedAt: Timestamp;
 }
+
+export const UserCollection = 'user';
