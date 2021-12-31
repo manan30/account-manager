eec13ae0f89646c43c4800ee01aaa6a93f5e1bea
Manan Joshi <mananjoshi1995@gmail.com>
2021-12-31 02:47:59 -0500

    Creating mobile bottom nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { LogoutIcon } from '@heroicons/react/outline';
-import cn from 'classnames';
+// import { Link, useLocation } from 'react-router-dom';
+// import cn from 'classnames';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useGlobalState } from '../../providers/GlobalStateProvider';
-import { useLogout } from '../../services/firebase/hooks/useLogout';
+// import { useGlobalState } from '../../providers/GlobalStateProvider';
 import { useWindowSize } from '../../hooks/Device/useWindowSize';
-
-const links = [
-  { to: '/accounts', linkText: 'Accounts' },
-  { to: '/expenses', linkText: 'Expenses' },
-  { to: '/creditors', linkText: 'Creditors' },
-  { to: '/recurring', linkText: 'Recurring' },
-  { to: '/profile', linkText: 'Profile' }
-];
+import Mobile from './Mobile';
 
 const Nav = () => {
-  const { pathname } = useLocation();
-  const { user } = useGlobalState();
-  const { logout } = useLogout();
+  // const { pathname } = useLocation();
+  // const { user } = useGlobalState();
   const windowSize = useWindowSize();
 
-  const isMobile = windowSize.width && windowSize.width < 640;
+  const isMobile = windowSize.width && windowSize.width < 768;
 
-  const [scrolled, setScrolled] = useState(false);
+  const [, setScrolled] = useState(false);
 
   useEffect(() => {
     const mainElement = document.querySelector('main');
@@ -42,49 +32,28 @@ const Nav = () => {
     return () => mainElement?.removeEventListener('scroll', scrollHandler);
   }, []);
 
-  const renderLinks = () => {
-    return links
-      .map((link) => {
-        return link.linkText !== 'Profile' ||
-          (link.linkText === 'Profile' && user) ? (
-          <Link
-            key={link.linkText}
-            to={link.to}
-            className={cn(
-              'text-base',
-              'hover:opacity-100',
-              pathname !== link.to && 'opacity-50'
-            )}
-          >
-            <li>{link.linkText}</li>
-          </Link>
-        ) : null;
-      })
-      .filter(Boolean);
-  };
+  // const renderLinks = () => {
+  //   return links
+  //     .map((link) => {
+  //       return link.linkText !== 'Profile' ||
+  //         (link.linkText === 'Profile' && user) ? (
+  //         <Link
+  //           key={link.linkText}
+  //           to={link.to}
+  //           className={cn(
+  //             'text-base',
+  //             'hover:opacity-100',
+  //             pathname !== link.to && 'opacity-50'
+  //           )}
+  //         >
+  //           <li>{link.linkText}</li>
+  //         </Link>
+  //       ) : null;
+  //     })
+  //     .filter(Boolean);
+  // };
 
-  return (
-    <nav
-      className={cn(
-        'flex items-center transition-shadow z-50 bg-indigo-600 text-gray-100',
-        scrolled && 'shadow-xl'
-      )}
-    >
-      <Link to='/'>
-        <h1 className='text-xl font-medium'>Account Manager</h1>
-      </Link>
-      {!isMobile ? (
-        <ul className='flex items-center ml-auto space-x-3'>
-          {renderLinks()}
-          {user ? (
-            <button onClick={logout} className='text-base'>
-              <LogoutIcon className='h-5 w-5 opacity-50 hover:opacity-100' />
-            </button>
-          ) : null}
-        </ul>
-      ) : null}
-    </nav>
-  );
+  return isMobile ? <Mobile /> : null;
 };
 
 export default Nav;
